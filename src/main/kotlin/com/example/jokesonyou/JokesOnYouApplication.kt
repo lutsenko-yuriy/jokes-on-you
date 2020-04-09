f75bb57ceb4fb84eaa0ce8b1da4a2d7fe0763ee3
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2020-04-09 23:53:35 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/jokesonyou/JokesOnYouApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jokesonyou
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JokesOnYouApplication
+
+fun main(args: Array<String>) {
+    runApplication<JokesOnYouApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jokesonyou/JokesOnYouApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jokesonyou
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JokesOnYouApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
